1f434e8ee5e07036a32bd311951b43268ea044b3
0haris0 <haris.valj@gmail.com>
2025-03-19 00:31:09 +0100

    Modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,6 @@ const Footer = () =>
               <NewsletterForm/>
             </div>
           </div>
-        </div>
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <Link href={"/"} aria-label="Home">
             <Logo className="h-8" fillOnHover>
